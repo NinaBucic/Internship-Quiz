b61997e876f7d62019ca8d27c1854c6faa9f7434
Nina Bucic <bucicnina@gmail.com>
2025-04-22 21:41:39 +0200

    Set up Turborepo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
