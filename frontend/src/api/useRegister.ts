5e38c239be026c995de9ef85d6110195da0e7783
Nina Bucic <bucicnina@gmail.com>
2025-04-23 15:04:02 +0200

    Implement useRegister hook

---
+++ frontend/src/api/useRegister.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { REGISTER_USER_PATH } from "../constants";
+import { JwtResponse, RegisterData } from "../types";
+import { api } from "./base";
+import toast from "react-hot-toast";
+
+const registerUser = (registerData: RegisterData) => {
+  return api.post<RegisterData, JwtResponse>(REGISTER_USER_PATH, registerData);
+};
+
+export const useRegister = () =>
+  useMutation({
+    mutationKey: ["register-user"],
+    mutationFn: registerUser,
+    onSuccess: (data: JwtResponse) => {
+      localStorage.setItem("jwt", JSON.stringify(data.access_token));
+      toast.success("Registration successful!");
+    },
+    onError: (error: any) => {
+      toast.error(error?.message || "Registration failed!");
+    },
+  });
--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,3 +1,5 @@
 export const BASE_URL = "http://localhost:3000/api";
 
 export const LOGIN_USER_PATH = "/auth/login";
+
+export const REGISTER_USER_PATH = "/auth/register";
