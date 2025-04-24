aeaa7ad1ee0ebabbe3533aa736e09cbe2aa5603d
Nina Bucic <bucicnina@gmail.com>
2025-04-24 17:37:35 +0200

    Update HomePage to show different content based on login status

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,6 +6,17 @@ import QuizImage from "../../assets/images/quiz_time.png";
 export const Home = () => {
   const navigate = useNavigate();
 
+  const token = localStorage.getItem("jwt");
+  const isLoggedIn = !!token;
+
+  const handleClick = () => {
+    if (isLoggedIn) {
+      navigate(ROUTES.QUIZZES_PAGE);
+    } else {
+      navigate(ROUTES.LOGIN);
+    }
+  };
+
   return (
     <Box
       display="flex"
@@ -26,18 +37,14 @@ export const Home = () => {
         }}
       />
       <Typography variant="h4" gutterBottom>
-        Welcome to the Ultimate Quiz Site!
+        {isLoggedIn ? "Welcome back!" : "Welcome to the Ultimate Quiz Site!"}
       </Typography>
       <Typography variant="body1" sx={{ maxWidth: 400, mb: 4 }}>
         Test your knowledge across various categories, colect points, and become
         a quiz master.
       </Typography>
-      <Button
-        variant="contained"
-        size="large"
-        onClick={() => navigate(ROUTES.LOGIN)}
-      >
-        Let's Go!
+      <Button variant="contained" size="large" onClick={handleClick}>
+        {isLoggedIn ? "Go to Quizzes" : "Login / Register"}
       </Button>
     </Box>
   );
